529584a918932502f439fd5611854b9091f2928a
Paul Guevarra <p.a.guevarra@gmail.com>
2017-07-14 11:31:14 -0700

    convert string to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 //back end
 function toChange(userIn){
   var output = "";
+  // var parsInput = parsInt(userIn)
+  var myNum = [];
+  var arrayStr = splitString(myNum, space);
+
+function  splitString(myNum, space){
+  var arrayString = stringToSplit.split(space);
+};
+
+
+  /*countform*/
   for(var i=1; i<=userIn; i++){
-    output += i
-  }
+    myNum += i + " "
+  };
 
 
+//   /*substitutionform*/
+//   for(var i=0; i<=pNum; i++){
+// alert(i)
+//     if (i%3 === 0) {
+//       output += "ping"
+//     } else {
+//       output += i
+//     }
+//   };
+  //
 
   return output;
-}
+};
 
 
 
