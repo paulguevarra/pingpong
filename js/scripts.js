ac8842fac4c28aa75328a8f233efba65378ca8af
Paul Guevarra <p.a.guevarra@gmail.com>
2017-07-14 13:41:53 -0700

    change html input form to number and reset back end JS to count to input number

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,16 @@
 //back end
 function toChange(userIn){
   var output = "";
-  // var parsInput = parsInt(userIn)
-  var myNum = [];
-  var arrayStr = splitString(myNum, space);
-
-function  splitString(myNum, space){
-  var arrayString = stringToSplit.split(space);
-};
-
 
   /*countform*/
   for(var i=1; i<=userIn; i++){
-    myNum += i + " "
+
+      output += i + " ";
+    }
   };
-
-
-//   /*substitutionform*/
-//   for(var i=0; i<=pNum; i++){
-// alert(i)
-//     if (i%3 === 0) {
-//       output += "ping"
-//     } else {
-//       output += i
-//     }
-//   };
-  //
-
   return output;
+
 };
-
-
 
 //front end
 $(document).ready(function(){
